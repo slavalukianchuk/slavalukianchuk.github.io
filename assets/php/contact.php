be62642e7c18422ba3316aa74adeb2e72178e517
slavalukianchuk <e.khalilovich@mail.ru>
2018-02-10 23:09:16 +0200

    Add files via upload

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -5,10 +5,10 @@ if(isset($_POST['message'])){
 	$name = $_POST['name'];
 	$email = $_POST['email'];
 	$message = $_POST['message'];
-    
-	
-	$to      = 'you@example.com';
-	$subject = 'Site Contact Form';
+
+
+	$to      = 'work.lukianchuk@gmail.com';
+	$subject = 'Business letter';
 
 	$headers = 'From: '. $email . "\r\n" .
     'Reply-To: '. $email . "\r\n" .
@@ -16,17 +16,17 @@ if(isset($_POST['message'])){
 
 	$status = mail($to, $subject, $message, $headers);
 
-	if($status == TRUE){	
+	if($status == TRUE){
 		$res['sendstatus'] = 'done';
-	
+
 		//Edit your message here
 		$res['message'] = 'Form Submission Successful';
     }
 	else{
 		$res['message'] = 'Failed to send mail. Please mail me to you@example.com';
 	}
-	
-	
+
+
 	echo json_encode($res);
 }
 
